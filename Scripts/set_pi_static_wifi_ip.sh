6d458ab3450451918823e374d8929bde324cea65
luisfdsousa <luisfdsousa@gmail.com>
2020-03-20 15:41:06 +0100

    added extra instructions

--- Scripts/set_pi_static_wifi_ip.sh
+++ Scripts/set_pi_static_wifi_ip.sh
@@ -1,9 +1,11 @@
-#Start by editing the dhcpcd.conf file
+#Set a static IP in 3 steps:
+
+# 1: Start by editing the dhcpcd.conf file
 
 sudo nano /etc/dhcpcd.conf
 
-# Edit / add the following lines at the bottom of the file
-# Note to specify the interface
+# 2: Edit / add the following lines at the bottom of the file
+#    Note to specify the interface
 
 	#interface eth0
 
@@ -16,3 +18,10 @@ sudo nano /etc/dhcpcd.conf
 	#static ip_address=192.168.0.200/24
 	#static routers=192.168.0.1
 	#static domain_name_servers=192.168.0.1
+
+#To exit the editor, press ctrl+x
+#To save your changes press the letter “Y” then hit enter
+
+# 3: Reboot and verify if the configuration was successful 
+reboot
+ifconfig
