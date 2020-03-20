3d1f9aad79664ca1246ae538b12f282cebda6813
luisfdsousa <luisfdsousa@gmail.com>
2020-03-20 15:38:46 +0100

    edited the static ip code/tutorial

--- Scripts/set_pi_static_wifi_ip.sh
+++ Scripts/set_pi_static_wifi_ip.sh
@@ -1,2 +1,18 @@
-hello world
-FCT-UNL
+#Start by editing the dhcpcd.conf file
+
+sudo nano /etc/dhcpcd.conf
+
+# Edit / add the following lines at the bottom of the file
+# Note to specify the interface
+
+	#interface eth0
+
+	#static ip_address=192.168.0.10/24
+	#static routers=192.168.0.1
+	#static domain_name_servers=192.168.0.1
+
+	#interface wlan0
+
+	#static ip_address=192.168.0.200/24
+	#static routers=192.168.0.1
+	#static domain_name_servers=192.168.0.1
